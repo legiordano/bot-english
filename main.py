09f71d8ac2a3c9820fdbb2bb9afa5e8c77417187
legiordano <lucasgiordano2219@gmail.com>
2023-07-17 18:45:29 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import pyttsx3
+
+engine = pyttsx3.init()
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+speak("Hello, how can I help you?")
+
+user_input = input(">> ")
+speak(f"You said: {user_input}")
+
+speak("Nice talking to you!")
